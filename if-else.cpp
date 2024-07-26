29bd7aeafb9763fb0e03ed622e540863e2b625a7
Akshat Yadav <akshatyadav1207@gmail.com>
2024-07-26 18:02:50 +0530

    a

--- if-else.cpp
+++ if-else.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main() {
     int x = 62, y = 62;
-    if (x > y)
+    if (x > y){
         cout << "x is greater";
-    else (y > x)
+        }
+    else if(y > x){
         cout << "y is greater";
-    else 
+        }
+    else{ 
         cout << "both are equal";
+        }
 }
