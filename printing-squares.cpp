420af5f358bd2ba37e36e062463e40186a94f676
Akshat Yadav <akshatyadav1207@gmail.com>
2024-08-01 11:39:20 +0530

    Create printing-squares.cpp

---
+++ printing-squares.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    int i = 1;
+    while(i<x+1){
+        cout << i*i << " ";
+        i++;
+    }
+     
+    return 0;
+}
